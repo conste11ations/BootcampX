d21baae88d19679560cea64a1c207a9536ae278f
Rachel Kat <rachelkat@gmail.com>
2020-04-30 00:42:37 +0000

    bootcamp_app worked with node_postgreSQL

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,25 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+
+pool.query(`
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name like '%${process.argv[2]}%'
+ORDER BY teacher;
+`)
+.then(console.log('connected'))
+.then(res => {
+  res.rows.forEach(teacher => {
+    console.log(`${teacher.cohort}: ${teacher.teacher}`);
+  })
+}).catch(err => console.error('query error', err.stack));
